ba50dd2fe2a32c00f22858615cf2b05d056f8c6b
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-09-25 11:55:32 -0500

    Update query

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from db_connection import DatabaseConnection
 def main():
     db = DatabaseConnection()
     db.connect()
-    results = db.execute_query('SELECT * FROM users')
+    results = db.execute_query('SELECT u.id, u.username, u.password, u.email, r.name FROM api_test_report.users u JOIN api_test_report.rol r ON u.id_rol = r.id')
     db.close()
     print(results)
 
